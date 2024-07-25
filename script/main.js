f2c8fd3524384a34ecccb5a52d7c4000b13d704b
qiyommasa <mujahidilqirom123@gmail.com>
2024-07-25 21:30:50 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Pengan ada musiknya nggak????',
+        title: 'Pengen ada musiknya nggak????',
         // text: "You won't be able to revert this!",
         icon: 'warning',
         showCancelButton: true,
