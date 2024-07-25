455dd59440285bee9acfbec968620ce5d83fd761
qiyommasa <mujahidilqirom123@gmail.com>
2024-07-25 21:20:22 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -200,9 +200,9 @@ const animationTimeline = () => {
         "-=2"
     )
     .from(".hat", 0.5, {
-        x: -100,
+        x: -70,
         y: 350,
-        rotation: -180,
+        rotation: -130,
         opacity: 0,
     })
     .staggerFrom(
